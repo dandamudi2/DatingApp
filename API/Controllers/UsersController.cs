e0f768867c53cf9fb429231248ade6299eb43cca
srinivasu <srinivasd2mail@gmail.com>
2021-02-09 23:37:27 +0800

    InitialCommit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using API.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Controllers
+{
+    
+    public class UsersController: BaseApiController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context){
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        
+        public async Task<ActionResult<AppUser>> GetUserOnId(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+
+
+    } 
+}
